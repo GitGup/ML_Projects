dc47c9eef8b992149cc6754d8413b71c4d0c65c5
Gup <gups.singhs@gmail.com>
2023-03-04 20:10:30 -0800

    Delete VAE.py

--- VAE.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
-
-print(train_images.shape())
-
-def process_images(images):
-    images = images.reshape()
